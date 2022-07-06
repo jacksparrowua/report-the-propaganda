fdea8911532f1407ca3df457e53bc115c32a0e56
Dmytro Doroshenko <dimdoroshenko@gmail.com>
2022-07-06 23:38:22 +0300

    refactoring: isort + black

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import random
 
-from report_message import ReportMessage, ButtonAction
+from report_message import ButtonAction, ReportMessage
 
 try:
     import colorama
+
     colorama.init()
 except ImportError:
     raise
@@ -11,17 +12,18 @@ except ImportError:
 import asyncio
 import time
 
-from telethon import TelegramClient, functions, types
-from config import api_id, api_hash
 from colorama import Fore
+from telethon import TelegramClient, functions, types
 
-channel_name = 'stopdrugsbot'
-start_new_report_message = '🚩Надіслати скаргу на ресурс'
+from config import api_hash, api_id
+
+channel_name = "stopdrugsbot"
+start_new_report_message = "🚩Надіслати скаргу на ресурс"
 
 
 async def main():
     count = 0
-    client = TelegramClient('session_name', api_id, api_hash)
+    client = TelegramClient("session_name", api_id, api_hash)
     await client.start()
 
     while True:
@@ -41,22 +43,26 @@ async def main():
                     response = await client(request)
                     channel_to_be_reported_id = response.chat.id
 
-                    print('#{} Reporting channel by invite link {} with id {}. Reason: {}'.format(
-                        count, report.get_channel_name(), channel_to_be_reported_id, report.get_report_reason())
+                    print(
+                        "#{} Reporting channel by invite link {} with id {}. Reason: {}".format(
+                            count, report.get_channel_name(), channel_to_be_reported_id, report.get_report_reason()
+                        )
                     )
                 else:
                     channel_to_be_reported = await client.get_entity(report.get_channel_name())
                     channel_to_be_reported_id = channel_to_be_reported.id
 
-                    print('#{} Reporting channel with name {} and id {}. Reason: {}'.format(
-                        count, report.get_channel_name(), channel_to_be_reported_id, report.get_report_reason())
+                    print(
+                        "#{} Reporting channel with name {} and id {}. Reason: {}".format(
+                            count, report.get_channel_name(), channel_to_be_reported_id, report.get_report_reason()
+                        )
                     )
 
                 request = functions.messages.ReportRequest(
-                    peer='username',
+                    peer="username",
                     id=[channel_to_be_reported_id],
                     reason=types.InputReportReasonOther(),
-                    message=report.get_report_reason()
+                    message=report.get_report_reason(),
                 )
 
                 response = await client(request)
@@ -77,14 +83,15 @@ async def main():
                     await report.click_button(ButtonAction.SKIP_TASK)
             except Exception as e:
                 print(f"{Fore.LIGHTRED_EX}{str(e)}{Fore.RESET}")
-                if 'A wait of' in str(e):
+                if "A wait of" in str(e):
                     exit()  # spam protection, just wait the time from the message
                 await report.click_button(ButtonAction.SKIP_TASK)
         else:
             print(f"{Fore.LIGHTYELLOW_EX}{'Skipping task, no telegram link in the description'}{Fore.RESET}")
             await report.click_button(ButtonAction.SKIP_TASK)
 
+
 asyncio.run(main())
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
