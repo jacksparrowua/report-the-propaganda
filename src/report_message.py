5aa28600548bcae3efcbf18456e79735ac2a6613
Dmytro Doroshenko <dimdoroshenko@gmail.com>
2022-07-13 00:26:12 +0300

    Features (#4)
    * refactoring: retrieve & report functions; feature: logger; refactoring: removed colors dependency
    
    * refactoring: removed default `None`s
    
    * fix: isort
    
    * feature: waiting for the unban
    
    * refactoring: moved `FloodWaitError` under the while True

--- src/report_message.py
+++ src/report_message.py
@@ -10,23 +10,16 @@ class ButtonAction(Enum):
 
 
 class ReportMessage:
-    __is_telegram = "t.me/"
-    __is_joinchat = "joinchat/"
-    __message = None
-    __message_body = None
-    __message_items = None
-    __message_buttons = None
-
-    def __init__(self, message: Message):
+    def __init__(self, message: Message) -> None:
         self.__message = message
         self.__message_body = message.message
         self.__message_items = self.__message_body.split("\n")
 
     def is_telegram(self) -> bool:
-        return True if self.__is_telegram in self.__message_body else False
+        return "t.me/" in self.__message_body
 
     def is_joinchat(self) -> bool:
-        return True if self.__is_joinchat in self.__message_body else False
+        return "joinchat/" in self.__message_body
 
     def get_report_reason(self) -> str:
         return self.__message_items[3]
@@ -34,11 +27,12 @@ class ReportMessage:
     def get_channel_name(self) -> str:
         if "joinchat/" in self.__message_items[6]:
             return self.__message_items[6].split("joinchat/")[-1]
+
         name = self.__message_items[6].split("t.me/")[-1]
         if "/" in name:
             name = name.split("/")[0]
         return name
 
-    async def click_button(self, action: ButtonAction):
+    async def click_button(self, action: ButtonAction) -> None:
         buttons = await self.__message.get_buttons()
         await buttons[action.value][0].click()
